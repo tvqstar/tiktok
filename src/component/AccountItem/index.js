4572263f91841b15f0d5d9c0e2c6ca959a48c6a3
TVQ <tavanquyet2206@gmail.com>
2022-10-09 16:31:10 +0700

    tim kiem on roi

--- src/component/AccountItem/index.js
+++ src/component/AccountItem/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
@@ -5,22 +6,18 @@ import styles from './AccountItem.module.scss';
 import Images from '~/component/Images';
 
 const cx = classNames.bind(styles);
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Images
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/28c41afb73cfa841b4f70ae5cf7636bd~c5_100x100.jpeg?x-expires=1665237600&x-signature=8lrvYuLA6jYtvPhuflpMKxOA30w%3D"
-                alt="TVQ"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Images className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <p className={cx('name')}>
-                    <span>Ta Van Quyet</span>
-                    <FontAwesomeIcon className={cx('name-icon')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('name-icon')} icon={faCheckCircle} />}
                 </p>
-                <span className={cx('username')}>tavanquyet</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/component/Layout/components/Search/index.js
+++ src/component/Layout/components/Search/index.js
@@ -6,6 +6,7 @@ import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import { Wrapper as PopperWrapper } from '~/component/Popper';
 import AccountItem from '~/component/AccountItem';
+import { useDebounce } from '~/hooks';
 import { SearchIcon } from '~/component/Icons';
 import styles from './Search.module.scss';
 
@@ -15,14 +16,30 @@ function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const debounced = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1, 2, 3]);
-        }, 0);
-    }, []);
+        if (!debounced.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        setLoading(true);
+
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
+            .then((res) => res.json())
+            .then((res) => {
+                setSearchResult(res.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log('Loi');
+            });
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');
@@ -42,10 +59,9 @@ function Search() {
                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
+                        {searchResult.map((result) => (
+                            <AccountItem key={result.id} data={result} />
+                        ))}
                     </PopperWrapper>
                 </div>
             )}
@@ -60,12 +76,12 @@ function Search() {
                     onChange={(e) => setSearchValue(e.target.value)}
                     onFocus={() => setShowResult(true)}
                 />
-                {!!searchValue && (
+                {!!searchValue && !loading && (
                     <button className={cx('search-clear-btn')} onClick={handleClear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                 )}
-                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                {!!loading && <FontAwesomeIcon className={cx('loading-icon')} icon={faSpinner} />}
 
                 <button className={cx('search-btn')}>
                     <SearchIcon />
